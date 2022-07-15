ef570dabd7a0cd7ba5013ab4b178c7b420d63544
Yunus Emre GÜRLÜ <yunusemregurlu@gmail.com>
2022-07-15 04:54:38 +0300

    addTodo func and react-redux add

--- todo_case/client/src/components/TodoFooter/index.js
+++ todo_case/client/src/components/TodoFooter/index.js
@@ -1,154 +1,61 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function TodoFooter() {
+  const todos = useSelector((state) => state.todos);
+
+  const active = localStorage.getItem('class');
+  const filteredYellow = todos.filter((todo) => todo.class === 'yellow');
+  const filteredPurple = todos.filter((todo) => todo.class === 'purple');
+  const filteredAqua = todos.filter((todo) => todo.class === 'aqua');
+  const filteredBrown = todos.filter((todo) => todo.class === 'brown');
+  const filteredOrange = todos.filter((todo) => todo.class === 'orange');
+
   return (
-    <footer className='footer'>
-      <div className='listItem   ' style={{ backgroundColor: 'yellow' }}>
-        <h3>Todo List 1</h3>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas rem,
-          unde earum ad harum magni necessitatibus hic molestiae repellendus quo
-          eveniet delectus eaque iste adipisci illo quasi officiis minus?
-          Libero, saepe distinctio quam in enim quod quo aperiam dolores
-          quaerat, cupiditate maxime! Provident alias ipsum quam natus commodi
-          modi laborum corporis accusantium vel sunt, rerum ea rem ab excepturi
-          magnam in, consectetur voluptates. Mollitia eos eaque tempora.
-          Cupiditate tempora culpa beatae hic rerum voluptates sint delectus
-          laboriosam, dolor iste explicabo deserunt vitae sequi praesentium non
-          corporis officia mollitia neque alias accusamus soluta dolorum quo.
-          Nemo, accusamus officiis distinctio at laborum tenetur ipsa
-          perferendis quidem magni rerum, eaque, temporibus assumenda dolorem
-          consectetur ratione explicabo natus repellendus molestias! Voluptate,
-          perferendis dolorum velit aut consequuntur minima repudiandae fugit
-          libero, doloribus assumenda consequatur. Quae ad assumenda praesentium
-          perferendis asperiores aut voluptatibus similique voluptates
-          voluptatem inventore, at delectus mollitia itaque quaerat illum ipsam
-          sed vel minus, obcaecati dolorem nesciunt, veniam beatae consectetur?
-          Ex veniam quasi perspiciatis provident ab reprehenderit optio placeat
-          nam aspernatur ipsam. Quis, sint deleniti, deserunt quidem dolorem
-          porro, necessitatibus impedit molestias nobis laborum consequatur
-          incidunt. Nobis similique consectetur ab debitis. Alias, saepe?
-          Blanditiis esse dolorem dolore laudantium, debitis reprehenderit
-          aliquam placeat illo.
-        </p>
-      </div>
-      <div className='listItem' style={{ backgroundColor: 'purple' }}>
-        <h3>Todo List 2</h3>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas rem,
-          unde earum ad harum magni necessitatibus hic molestiae repellendus quo
-          eveniet delectus eaque iste adipisci illo quasi officiis minus?
-          Libero, saepe distinctio quam in enim quod quo aperiam dolores
-          quaerat, cupiditate maxime! Provident alias ipsum quam natus commodi
-          modi laborum corporis accusantium vel sunt, rerum ea rem ab excepturi
-          magnam in, consectetur voluptates. Mollitia eos eaque tempora.
-          Cupiditate tempora culpa beatae hic rerum voluptates sint delectus
-          laboriosam, dolor iste explicabo deserunt vitae sequi praesentium non
-          corporis officia mollitia neque alias accusamus soluta dolorum quo.
-          Nemo, accusamus officiis distinctio at laborum tenetur ipsa
-          perferendis quidem magni rerum, eaque, temporibus assumenda dolorem
-          consectetur ratione explicabo natus repellendus molestias! Voluptate,
-          perferendis dolorum velit aut consequuntur minima repudiandae fugit
-          libero, doloribus assumenda consequatur. Quae ad assumenda praesentium
-          perferendis asperiores aut voluptatibus similique voluptates
-          voluptatem inventore, at delectus mollitia itaque quaerat illum ipsam
-          sed vel minus, obcaecati dolorem nesciunt, veniam beatae consectetur?
-          Ex veniam quasi perspiciatis provident ab reprehenderit optio placeat
-          nam aspernatur ipsam. Quis, sint deleniti, deserunt quidem dolorem
-          porro, necessitatibus impedit molestias nobis laborum consequatur
-          incidunt. Nobis similique consectetur ab debitis. Alias, saepe?
-          Blanditiis esse dolorem dolore laudantium, debitis reprehenderit
-          aliquam placeat illo.
-        </p>
-      </div>
-      <div className='listItem' style={{ backgroundColor: 'aqua' }}>
-        <h3>Todo List 3</h3>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas rem,
-          unde earum ad harum magni necessitatibus hic molestiae repellendus quo
-          eveniet delectus eaque iste adipisci illo quasi officiis minus?
-          Libero, saepe distinctio quam in enim quod quo aperiam dolores
-          quaerat, cupiditate maxime! Provident alias ipsum quam natus commodi
-          modi laborum corporis accusantium vel sunt, rerum ea rem ab excepturi
-          magnam in, consectetur voluptates. Mollitia eos eaque tempora.
-          Cupiditate tempora culpa beatae hic rerum voluptates sint delectus
-          laboriosam, dolor iste explicabo deserunt vitae sequi praesentium non
-          corporis officia mollitia neque alias accusamus soluta dolorum quo.
-          Nemo, accusamus officiis distinctio at laborum tenetur ipsa
-          perferendis quidem magni rerum, eaque, temporibus assumenda dolorem
-          consectetur ratione explicabo natus repellendus molestias! Voluptate,
-          perferendis dolorum velit aut consequuntur minima repudiandae fugit
-          libero, doloribus assumenda consequatur. Quae ad assumenda praesentium
-          perferendis asperiores aut voluptatibus similique voluptates
-          voluptatem inventore, at delectus mollitia itaque quaerat illum ipsam
-          sed vel minus, obcaecati dolorem nesciunt, veniam beatae consectetur?
-          Ex veniam quasi perspiciatis provident ab reprehenderit optio placeat
-          nam aspernatur ipsam. Quis, sint deleniti, deserunt quidem dolorem
-          porro, necessitatibus impedit molestias nobis laborum consequatur
-          incidunt. Nobis similique consectetur ab debitis. Alias, saepe?
-          Blanditiis esse dolorem dolore laudantium, debitis reprehenderit
-          aliquam placeat illo.
-        </p>
-      </div>
-      <div className='listItem' style={{ backgroundColor: 'brown' }}>
-        <h3>Todo List 4</h3>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas rem,
-          unde earum ad harum magni necessitatibus hic molestiae repellendus quo
-          eveniet delectus eaque iste adipisci illo quasi officiis minus?
-          Libero, saepe distinctio quam in enim quod quo aperiam dolores
-          quaerat, cupiditate maxime! Provident alias ipsum quam natus commodi
-          modi laborum corporis accusantium vel sunt, rerum ea rem ab excepturi
-          magnam in, consectetur voluptates. Mollitia eos eaque tempora.
-          Cupiditate tempora culpa beatae hic rerum voluptates sint delectus
-          laboriosam, dolor iste explicabo deserunt vitae sequi praesentium non
-          corporis officia mollitia neque alias accusamus soluta dolorum quo.
-          Nemo, accusamus officiis distinctio at laborum tenetur ipsa
-          perferendis quidem magni rerum, eaque, temporibus assumenda dolorem
-          consectetur ratione explicabo natus repellendus molestias! Voluptate,
-          perferendis dolorum velit aut consequuntur minima repudiandae fugit
-          libero, doloribus assumenda consequatur. Quae ad assumenda praesentium
-          perferendis asperiores aut voluptatibus similique voluptates
-          voluptatem inventore, at delectus mollitia itaque quaerat illum ipsam
-          sed vel minus, obcaecati dolorem nesciunt, veniam beatae consectetur?
-          Ex veniam quasi perspiciatis provident ab reprehenderit optio placeat
-          nam aspernatur ipsam. Quis, sint deleniti, deserunt quidem dolorem
-          porro, necessitatibus impedit molestias nobis laborum consequatur
-          incidunt. Nobis similique consectetur ab debitis. Alias, saepe?
-          Blanditiis esse dolorem dolore laudantium, debitis reprehenderit
-          aliquam placeat illo.
-        </p>
-      </div>
-      <div className='listItem' style={{ backgroundColor: 'orange' }}>
-        <h3>Todo List 5</h3>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas rem,
-          unde earum ad harum magni necessitatibus hic molestiae repellendus quo
-          eveniet delectus eaque iste adipisci illo quasi officiis minus?
-          Libero, saepe distinctio quam in enim quod quo aperiam dolores
-          quaerat, cupiditate maxime! Provident alias ipsum quam natus commodi
-          modi laborum corporis accusantium vel sunt, rerum ea rem ab excepturi
-          magnam in, consectetur voluptates. Mollitia eos eaque tempora.
-          Cupiditate tempora culpa beatae hic rerum voluptates sint delectus
-          laboriosam, dolor iste explicabo deserunt vitae sequi praesentium non
-          corporis officia mollitia neque alias accusamus soluta dolorum quo.
-          Nemo, accusamus officiis distinctio at laborum tenetur ipsa
-          perferendis quidem magni rerum, eaque, temporibus assumenda dolorem
-          consectetur ratione explicabo natus repellendus molestias! Voluptate,
-          perferendis dolorum velit aut consequuntur minima repudiandae fugit
-          libero, doloribus assumenda consequatur. Quae ad assumenda praesentium
-          perferendis asperiores aut voluptatibus similique voluptates
-          voluptatem inventore, at delectus mollitia itaque quaerat illum ipsam
-          sed vel minus, obcaecati dolorem nesciunt, veniam beatae consectetur?
-          Ex veniam quasi perspiciatis provident ab reprehenderit optio placeat
-          nam aspernatur ipsam. Quis, sint deleniti, deserunt quidem dolorem
-          porro, necessitatibus impedit molestias nobis laborum consequatur
-          incidunt. Nobis similique consectetur ab debitis. Alias, saepe?
-          Blanditiis esse dolorem dolore laudantium, debitis reprehenderit
-          aliquam placeat illo.
-        </p>
-      </div>
-    </footer>
+    todos.length > 0 && (
+      <footer className='footer'>
+        {filteredYellow.length > 0 && (
+          <div className='listItem' style={{ backgroundColor: 'yellow' }}>
+            <h3>Todo List 1</h3>
+            {filteredYellow.map((todo, index) => (
+              <p key={index}>{todo.title}</p>
+            ))}
+          </div>
+        )}
+        {filteredPurple.length > 0 && (
+          <div className='listItem' style={{ backgroundColor: 'purple' }}>
+            <h3>Todo List 2</h3>
+            {filteredPurple.map((todo, index) => (
+              <p key={index}>{todo.title}</p>
+            ))}
+          </div>
+        )}
+        {filteredAqua.length > 0 && (
+          <div className='listItem' style={{ backgroundColor: 'aqua' }}>
+            <h3>Todo List 3</h3>
+            {filteredAqua.map((todo, index) => (
+              <p key={index}>{todo.title}</p>
+            ))}
+          </div>
+        )}
+        {filteredBrown.length > 0 && (
+          <div className='listItem' style={{ backgroundColor: 'brown' }}>
+            <h3>Todo List 4</h3>
+            {filteredBrown.map((todo, index) => (
+              <p key={index}>{todo.title}</p>
+            ))}
+          </div>
+        )}
+        {filteredOrange.length > 0 && (
+          <div className='listItem' style={{ backgroundColor: 'orange' }}>
+            <h3>Todo List 5</h3>
+            {filteredOrange.map((todo, index) => (
+              <p key={index}>{todo.title}</p>
+            ))}
+          </div>
+        )}
+      </footer>
+    )
   );
 }
 
